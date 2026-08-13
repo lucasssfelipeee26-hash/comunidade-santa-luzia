fb161c988826b77c5be5fc7092ad33c23b847346
lucasssfelipeee26-hash <lucasssfelipeee26@gmail.com>
2026-08-13 09:50:41 -0400

    Cacheia indice anual da Liturgia offline

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,8 @@
-const CACHE = "santa-luzia-offline-v15"
+const CACHE = "santa-luzia-offline-v16"
 const ACERVO_BASE = "/offline/iliturgia/"
 const ACERVO = [
   `${ACERVO_BASE}manifest.json`,
+  `${ACERVO_BASE}indice-liturgico-2026.json`,
   `${ACERVO_BASE}catequeses.html.json.gz`,
   `${ACERVO_BASE}comentarios.html.json.gz`,
   ...Array.from({ length: 12 }, (_, i) => `${ACERVO_BASE}evangelhos-${String(i + 1).padStart(2, "0")}.html.json.gz`),
