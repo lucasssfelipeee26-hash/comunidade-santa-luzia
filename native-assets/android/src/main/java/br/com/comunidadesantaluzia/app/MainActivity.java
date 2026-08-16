bbbc4f720b61d3fd074e4b29b974b3c9a7c1c762
lucasssfelipeee26-hash <lucasssfelipeee26@gmail.com>
2026-08-16 08:12:39 -0400

    Fixa modo claro no shell Android

--- native-assets/android/src/main/java/br/com/comunidadesantaluzia/app/MainActivity.java
+++ native-assets/android/src/main/java/br/com/comunidadesantaluzia/app/MainActivity.java
@@ -1,11 +1,16 @@
 package br.com.comunidadesantaluzia.app;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        // O design do aplicativo é claro. Impede que o Android/Samsung injete
+        // automaticamente texto branco em popups, campos e menus de seleção.
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         registerPlugin(AppUpdaterPlugin.class);
         registerPlugin(CaminhoDaLuzPlugin.class);
         registerPlugin(OfflineStorePlugin.class);
