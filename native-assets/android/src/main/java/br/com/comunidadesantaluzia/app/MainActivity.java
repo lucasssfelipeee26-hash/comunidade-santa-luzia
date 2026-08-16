f1499140b001abb0cb115fe7fdfc22d966098407
lucasssfelipeee26-hash <lucasssfelipeee26@gmail.com>
2026-08-16 09:20:38 -0400

    Forca menu de selecao em modo primario no Android

--- native-assets/android/src/main/java/br/com/comunidadesantaluzia/app/MainActivity.java
+++ native-assets/android/src/main/java/br/com/comunidadesantaluzia/app/MainActivity.java
@@ -1,19 +1,39 @@
 package br.com.comunidadesantaluzia.app;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.ActionMode;
 import androidx.appcompat.app.AppCompatDelegate;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // O design do aplicativo é claro. Impede que o Android/Samsung injete
-        // automaticamente texto branco em popups, campos e menus de seleção.
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         registerPlugin(AppUpdaterPlugin.class);
         registerPlugin(CaminhoDaLuzPlugin.class);
         registerPlugin(OfflineStorePlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public ActionMode onWindowStartingActionMode(ActionMode.Callback callback, int type) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && type == ActionMode.TYPE_FLOATING) {
+            ActionMode mode = super.onWindowStartingActionMode(callback, ActionMode.TYPE_PRIMARY);
+            if (mode != null) {
+                mode.setType(ActionMode.TYPE_PRIMARY);
+            }
+            return mode;
+        }
+        return super.onWindowStartingActionMode(callback, type);
+    }
+
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && mode != null && mode.getType() == ActionMode.TYPE_FLOATING) {
+            mode.setType(ActionMode.TYPE_PRIMARY);
+        }
+        super.onActionModeStarted(mode);
     }
 }
