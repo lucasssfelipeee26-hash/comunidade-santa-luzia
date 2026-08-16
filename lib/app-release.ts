b44230bdac2b6e4f4452d1a86e4bdb70a738617c
lucasssfelipeee26-hash <lucasssfelipeee26@gmail.com>
2026-08-16 15:19:48 -0400

    Corrige metadados e repete build Android 1.0.6
    * Alinha versão pública com Android 1.0.6
    
    * Prepara metadados públicos para 1.0.6
    
    * Repete build Android 1.0.6 após alinhar metadados

--- lib/app-release.ts
+++ lib/app-release.ts
@@ -6,4 +6,4 @@
  * público mostrado no ecossistema do aplicativo.
  */
 export const APP_AUTH_RELEASE = "0.11.0"
-export const APP_DISPLAY_VERSION = "1.0.5"
+export const APP_DISPLAY_VERSION = "1.0.6"
