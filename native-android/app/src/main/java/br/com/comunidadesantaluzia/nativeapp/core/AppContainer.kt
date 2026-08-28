000e5938954385bfab011f35af18664915637126
lucasssfelipeee26-hash <lucasssfelipeee26@gmail.com>
2026-08-27 20:03:51 -0400

    feat(native): centraliza dependências Kotlin do Santa Luzia

---
+++ native-android/app/src/main/java/br/com/comunidadesantaluzia/nativeapp/core/AppContainer.kt
@@ -0,0 +1,18 @@
+package br.com.comunidadesantaluzia.nativeapp.core
+
+import android.content.Context
+import br.com.comunidadesantaluzia.nativeapp.core.audit.SantaLuziaAuditor
+import br.com.comunidadesantaluzia.nativeapp.core.data.NativeDatabase
+import br.com.comunidadesantaluzia.nativeapp.core.data.SantaLuziaRepository
+import br.com.comunidadesantaluzia.nativeapp.core.network.NativeHttpClient
+import br.com.comunidadesantaluzia.nativeapp.core.session.SessionStore
+
+internal class AppContainer(context: Context) {
+    private val appContext = context.applicationContext
+
+    val database = NativeDatabase(appContext)
+    val sessionStore = SessionStore(appContext)
+    val httpClient = NativeHttpClient(sessionStore)
+    val repository = SantaLuziaRepository(database, httpClient, sessionStore)
+    val auditor = SantaLuziaAuditor(appContext, database)
+}
